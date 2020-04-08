d3f2ba22197cd30dc380711d120507a12ea2df95
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-08 00:18:57 -0600

    Update routes and views to sign in with oauth from welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  root to: "home#index"
   root 'welcome#show'
 
 end
