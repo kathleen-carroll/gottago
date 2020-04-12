377345ff3ef6918732634b20bd5e6568a2250cab
Daniel <danmoran.pro@gmail.com>
2020-04-11 23:05:07 -0600

    updated sinatra. still WIP

--- locations_service/sinatra.rb
+++ locations_service/sinatra.rb
@@ -1,19 +1,32 @@
 require 'sinatra'
 require 'faraday'
+require 'pry'
 
 get '/bathrooms' do
-  number_of_results = params['number_of_results']
+  number_of_results = 10 #params['number_of_results']
   latitude = params['latitude'].to_f
   longitude = params['longitude'].to_f
-
   get_json(number_of_results, latitude, longitude).to_json
 end
 
 def get_json(number_of_results, latitude, longitude)
-  response = conn.get("/api/v1/restrooms/by_location?page=1&per_page=#{number_of_results}&offset=0&ada=false&unisex=false&lat=#{latitude}&lng=#{longitude}")
+  binding.pry
+  # response = conn.get(https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=39.750865&lng=-104.9966132)
+  # response = Faraday.get("https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=#{number_of_results}&offset=0&ada=false&unisex=false&lat=#{latitude}&lng=#{longitude}")
+  response = Faraday.get("by_location?page=1&per_page=10&offset=0&lat=39.750865&lng=-104.9966132"
+ 
+  # the below is something we may want to look into something like this. Could be easier for sinatra. However the above works.  
+    # Faraday.new(https://www.refugerestrooms.org/api/v1/restrooms/) do |c|
+    #   c.params["lat"] = 39.75
+    #   c.headers["key"] = ENV["KEY_VALUE"]
+    #   c.params['lng'] = 104.99
+    # end
+
   JSON.parse(response.body, symbolize_names: true)
 end
 
-def conn
-  Faraday.new(url: 'https://www.refugerestrooms.org/api/v1/restrooms')
-end
+# def conn
+#   Faraday.new(url: 'https://www.refugerestrooms.org/api/v1/restrooms')
+# end
+
+# 
