b8e0274f7b24c67dec1bbdd71cfd44a6b3f02710
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-12 22:51:24 -0600

    Update routes to destroy user when logging out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#show'
+  # namespace 'users' do
+  #   delete '/sign_out', to: 'sessions#destroy'
+  # end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",
+                                      :sessions => 'users/sessions'}
 
   resources :map, only: :index #, path: '/auth/facebook/callback' #'/map'#
 end
