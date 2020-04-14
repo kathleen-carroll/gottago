b2b547dd7a6a42b8a86abcde285487e9dd2e388f
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-14 12:57:05 -0600

    Update with bathroom instance variable

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,9 @@
 class MapController < ApplicationController
   def index
-    @user = User.find(session[:user_id]) #User.last
+    @user = User.find(session[:user_id])
     current = @user.address
     @user.update(lat: current[:location][:lat], long: current[:location][:lng])
+
+    @bathrooms = Bathroom.raw_data
   end
 end
