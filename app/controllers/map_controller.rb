3f8fc81371eb479de6446fbded15f5433aa67bc4
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-16 17:47:51 -0600

    Update users to not look for location

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,10 +2,10 @@ class MapController < ApplicationController
   def index
     @user = User.find(session[:user_id])
 
-    if @user.latitude.nil?
-      current = @user.address
-      @user.update(latitude: current[:location][:lat], longitude: current[:location][:lng])
-    end
+    # if @user.latitude.nil?
+    #   current = @user.address
+    #   @user.update(latitude: current[:location][:lat], longitude: current[:location][:lng])
+    # end
 
     @bathrooms = Bathroom.all
     #.include(:reviews)
