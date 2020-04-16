68f7b7fd073a6653f3fc777f2d7e1bc6e1710cd7
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-16 09:28:05 -0600

    Update users table

--- db/migrate/20200409210439_add_lat_long_to_users.rb
+++ db/migrate/20200409210439_add_lat_long_to_users.rb
@@ -1,6 +1,6 @@
 class AddLatLongToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :lat, :float
-    add_column :users, :long, :float
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema.define(version: 20200412224354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bathrooms", force: :cascade do |t|
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.float "latitude"
+    t.float "longitude"
+    t.boolean "accessible"
+    t.boolean "unisex"
+    t.boolean "changing_table"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,24 +41,10 @@ ActiveRecord::Schema.define(version: 20200412224354) do
     t.string "uid"
     t.string "name"
     t.text "image"
-    t.float "lat"
-    t.float "long"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-    create_table "bathrooms", force: :cascade do |t|
-      t.string "name"
-      t.string "street"
-      t.string "city"
-      t.string "state"
-      t.float "latitude"
-      t.float "longitude"
-      t.boolean "accessible"
-      t.boolean "unisex"
-      t.boolean "changing_table"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
   end
 
 end
