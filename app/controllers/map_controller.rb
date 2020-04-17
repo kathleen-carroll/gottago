e51b88789e1931dd6de9ef75c265f693256e3f1f
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-17 08:42:17 -0600

    Trying to grab current location

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,16 +1,20 @@
 class MapController < ApplicationController
   def index
+    # require "pry"; binding.pry
+
     if !current_user
       redirect_to root_path
     end
 
     @user = current_user #User.find(session[:user_id])
 
+    # current_user.update(latitude: params["lat"].to_f, longitude: params["lat"].to_f)
+
     if session[:advanced] == true
       @bathrooms = Bathroom.advanced_search(search_terms)
       session[:advanced] = false
     else
-      @bathrooms = Bathroom.all
+      @bathrooms = Bathroom.all[0..15]
     end
   end
 
